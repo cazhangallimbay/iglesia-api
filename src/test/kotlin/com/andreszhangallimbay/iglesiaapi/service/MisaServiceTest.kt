e05f30d3dbf57532270f77bf16c5eeb55d9e8ac2
cazhangallimbay <cazhangallimbay@sudamericano.edu.ec>
2022-01-21 11:56:02 -0500

    Deber con errores

---
+++ src/test/kotlin/com/andreszhangallimbay/iglesiaapi/service/MisaServiceTest.kt
@@ -0,0 +1,54 @@
+package com.andreszhangallimbay.iglesiaapi.service
+
+import com.andreszhangallimbay.iglesiaapi.model.Iglesia
+import com.andreszhangallimbay.iglesiaapi.model.Misa
+import com.andreszhangallimbay.iglesiaapi.repository.IglesiaRepository
+import com.andreszhangallimbay.iglesiaapi.repository.MisaRepository
+import com.google.gson.Gson
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.springframework.boot.test.context.SpringBootTest
+import java.io.File
+
+@SpringBootTest
+class MisaServiceTest {
+    @InjectMocks
+    lateinit var misaService: MisaService
+
+    @Mock
+    lateinit var misaRepository: MisaRepository
+
+    @Mock
+    lateinit var iglesiaRepository: IglesiaRepository
+
+    val jsonString = File("./src/test/resources/Misa/crearMisa.json").readText(Charsets.UTF_8)
+    val misaMock = Gson().fromJson(jsonString, Misa::class.java)
+
+    val jsonString = File("./src/test/resources/Iglesia/crearIglesia.json").readText(Charsets.UTF_8)
+    val iglesiaMock = Gson().fromJson(jsonString, Iglesia::class.java)
+
+    @Test
+    fun createMisa(){
+        //PAra actualizar
+        /// LLAVES  FORENEAS
+        Mockito.`when`(iglesiaRepository.findById(misaMock.iglesia_id)).thenReturn(iglesiaMock)
+
+        Mockito.`when`(misaRepository.save(Mockito.any(Misa::class.java))).thenReturn(misaMock)
+        val response = misaService.save(misaMock)
+        Assertions.assertEquals(response.id, misaMock.id)
+
+}
+
+    @Test
+    fun saveTeacherFailed(){
+        Assertions.assertThrows(Exception::class.java) {
+            misaMock.apply { fecha = "" }
+            Mockito.`when`(misaRepository.save(Mockito.any(Misa::class.java))).thenReturn(misaMock)
+            misaService.save(misaMock)
+        }
+    }
+
+}
